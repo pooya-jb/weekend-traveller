b8615b97319f8af423d965499246923ec9469d8c
DanielMaczak <daniel.maczak@proton.me>
2024-01-10 18:03:40 +0100

    feat(server): implement global try-catch logic
    implement global try-catch handler in router to safeguard the server against unhandled errors

--- server/src/controllers/flightData.controller.ts
+++ server/src/controllers/flightData.controller.ts
@@ -9,16 +9,12 @@ export const getCurrencies = async (
   res: Response,
   next: Function
 ): Promise<void> => {
-  try {
-    //  Request data
-    const data: libFd.Currencies | null = await model.getCurrencies();
-    if (data === null) next(new errors.BadGateway('No data received'));
-    //  Respond
-    res.status(200);
-    res.json(data);
-  } catch (err) {
-    next(new errors.InternalServerError(<Error>err));
-  }
+  //  Request data
+  const data: libFd.Currencies | null = await model.getCurrencies();
+  if (data === null) next(new errors.BadGateway('No data received'));
+  //  Respond
+  res.status(200);
+  res.json(data);
 };
 
 export const getAirports = async (): Promise<void> => {};
--- server/src/router.ts
+++ server/src/router.ts
@@ -5,12 +5,25 @@ import * as flightData from './controllers/flightData.controller.js';
 
 export const router: Router = express.Router();
 
+function catcher(fn: Function) {
+  return async (req: Request, res: Response, next: Function) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      if (!res.headersSent) next(new errors.InternalServerError(<Error>err));
+    }
+  };
+}
+
 router
-  .get('/currencies', flightData.getCurrencies)
-  .get('/airports', flightData.getAirports)
-  .post('/request-locale-info', flightData.postLocaleInfoRequest)
-  .post('/request-cheapest-flights', flightData.postCheapestFlightsRequest)
-  .post('/request-flight-info', flightData.postFlightInfoRequest)
+  .get('/currencies', catcher(flightData.getCurrencies))
+  .get('/airports', catcher(flightData.getAirports))
+  .post('/request-locale-info', catcher(flightData.postLocaleInfoRequest))
+  .post(
+    '/request-cheapest-flights',
+    catcher(flightData.postCheapestFlightsRequest)
+  )
+  .post('/request-flight-info', catcher(flightData.postFlightInfoRequest))
   .all('/*', (_: Request, __: Response, next: Function) => {
     next(new errors.NotFound(`Page doesn't exist.`));
   });
