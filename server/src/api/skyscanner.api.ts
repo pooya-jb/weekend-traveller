a8ac03d337d4c5b0d475143e308e1db8f4b6c7be
DanielMaczak <daniel.maczak@proton.me>
2024-01-15 15:21:13 +0100

    docs(server-api): add module commentary
    do module code review and clean-up; add module commentary

--- server/src/api/skyscanner.api.ts
+++ server/src/api/skyscanner.api.ts
@@ -1,52 +1,62 @@
+/**
+ * @module
+ * Imports from skyscanner API.
+ * All processes follow standard fetch pattern.
+ * All data processing is handled by model.
+ * API response code is checked;
+ * in all other cases relies on global error catcher.
+ * @version 1.0.0
+ * @link https://developers.skyscanner.net/docs/intro
+ * @link https://html.duckduckgo.com/html?q=skyscanner%20developer%20api
+ */
+
+//  Internal dependencies
 import { errors } from '../middleware/errorHandler.js';
 import * as libApi from '../libraries/skyscanner.api.js';
 
+//  Skyscanner API URL
 const BASE_URL: string = 'https://partners.api.skyscanner.net/apiservices/v3';
 
 export const getCurrencies = async (): Promise<libApi.Currencies> => {
-  //  Request flight data
   const url: string = `${BASE_URL}/culture/currencies`;
   const response = await fetch(url, {
     headers: { 'x-api-key': process.env.SKYSCANNER_API_KEY },
   });
-  //  Process response
-  if (!response.ok) throw new errors.BadGateway('API server responded notOk.');
+  if (!response.ok)
+    throw new errors.BadGateway(`API server responded ${response.status}.`);
   const data: libApi.Currencies = await response.json();
-  return data as libApi.Currencies;
+  return data;
 };
 
 export const getGeoHierarchy = async (
   locale: string
 ): Promise<libApi.GeoHierarchy> => {
-  //  Request flight data
   const url: string = `${BASE_URL}/geo/hierarchy/flights/${locale}`;
   const response = await fetch(url, {
     headers: { 'x-api-key': process.env.SKYSCANNER_API_KEY },
   });
-  //  Process response
-  if (!response.ok) throw new errors.BadGateway('API server responded notOk.');
+  if (!response.ok)
+    throw new errors.BadGateway(`API server responded ${response.status}.`);
   const data: libApi.GeoHierarchy = await response.json();
-  return data as libApi.GeoHierarchy;
+  return data;
 };
 
 export const getNearestCulture = async (
   ipAddress: string
 ): Promise<libApi.NearestCulture> => {
-  //  Request flight data
   const url: string = `${BASE_URL}/culture/nearestculture?ipAddress=${ipAddress}`;
   const response = await fetch(url, {
     headers: { 'x-api-key': process.env.SKYSCANNER_API_KEY },
   });
-  //  Process response
-  if (!response.ok) throw new errors.BadGateway('API server responded notOk.');
+  if (!response.ok)
+    throw new errors.BadGateway(`API server responded ${response.status}.`);
   const data: libApi.NearestCulture = await response.json();
-  return data as libApi.NearestCulture;
+  return data;
 };
 
 export const postFlightsIndicativeRequest = async (
   requestBody: libApi.FlightsIndicativeRequest
 ): Promise<libApi.FlightsIndicative> => {
-  //  Request flight data
   const url: string = `${BASE_URL}/flights/indicative/search`;
   const response = await fetch(url, {
     method: 'POST',
@@ -56,16 +66,15 @@ export const postFlightsIndicativeRequest = async (
     },
     body: JSON.stringify(requestBody),
   });
-  //  Process response
-  if (!response.ok) throw new errors.BadGateway('API server responded notOk.');
+  if (!response.ok)
+    throw new errors.BadGateway(`API server responded ${response.status}.`);
   const data: libApi.FlightsIndicative = await response.json();
-  return data as libApi.FlightsIndicative;
+  return data;
 };
 
 export const postFlightsLivePricesRequest = async (
   requestBody: libApi.FlightsLivePricesRequest
 ): Promise<libApi.FlightsLivePrices> => {
-  //  Request flight data
   const url: string = `${BASE_URL}/flights/live/search/create`;
   const response = await fetch(url, {
     method: 'POST',
@@ -75,8 +84,8 @@ export const postFlightsLivePricesRequest = async (
     },
     body: JSON.stringify(requestBody),
   });
-  //  Process response
-  if (!response.ok) throw new errors.BadGateway('API server responded notOk.');
+  if (!response.ok)
+    throw new errors.BadGateway(`API server responded ${response.status}.`);
   const data: libApi.FlightsLivePrices = await response.json();
-  return data as libApi.FlightsLivePrices;
+  return data;
 };
