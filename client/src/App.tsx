88f331e261b0b89ca798d4b95b8147defaa1f3fb
DanielMaczak <daniel.maczak@proton.me>
2024-01-15 07:38:51 +0100

    docs(client-App): add module commentary
    do module code review and clean-up; add module commentary

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,18 @@
+//  External dependencies
 import 'react-datepicker/dist/react-datepicker.css';
 import './App.css';
 import React, { useContext, useEffect, useState } from 'react';
 
+//  Internal dependencies
 import * as libFd from './libraries/flightData.service';
 import * as c from './services/const.service';
 import { postLocaleInfoRequest } from './services/api.service';
+import Header from './components/header.component';
 import FlightsDashboard from './components/flightsDashboard.component';
 import Footer from './components/footer.component';
-import Header from './components/header.component';
 
+//  Locale info is present in majority of API requests anywhere in app
+//  Using empty string as defaults is more convenient than undefined here
 const defaultLocaleInfo: libFd.LocaleInfo = {
   marketCode: '',
   locationName: '',
@@ -18,16 +22,23 @@ const defaultLocaleInfo: libFd.LocaleInfo = {
 export const LocaleContext: React.Context<libFd.LocaleInfo> =
   React.createContext(defaultLocaleInfo);
 
+/**
+ * Top-level app component.
+ * Keeps accurate locale info at all times.
+ */
 function App() {
-  const [localeInfo, setLocaleInfo] = useState(defaultLocaleInfo);
-  const [selectedCurrency, selectCurrency] = useState(
+  //  State hooks
+  const [localeInfo, setLocaleInfo] =
+    useState<libFd.LocaleInfo>(defaultLocaleInfo);
+  const [selectedCurrency, selectCurrency] = useState<string>(
     useContext(LocaleContext).currencyCode
   );
 
+  //  Data update hooks
   useEffect(() => {
     postLocaleInfoRequest().then(data => {
       setLocaleInfo({ ...data, localeCode: c.GLOBAL_LOCALE });
-      selectCurrency(data.currencyCode);
+      selectCurrency(data.currencyCode); // autoselect currency for user
     });
   }, []);
   useEffect(() => {
