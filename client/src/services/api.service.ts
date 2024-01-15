efa1575c4ca01e006863af044986913c9bbb705c
DanielMaczak <daniel.maczak@proton.me>
2024-01-15 08:37:13 +0100

    feat(client-api): implement try-catch login in API module
    implement logic to catch errors and provide null output when no data has been obtained from API calls

--- client/src/services/api.service.ts
+++ client/src/services/api.service.ts
@@ -1,96 +1,141 @@
+//  Internal dependencies
 import * as libFd from '../libraries/flightData.service';
 import * as c from '../services/const.service';
 
+//  Data cache
 let currencies: libFd.Currencies = [];
 let airports: libFd.Airports;
 let localeInfo: libFd.LocaleInfo;
 let cheapestFlightsCache: { [key: string]: libFd.CheapestFlights } = {};
+let flightInfoCache: { [key: string]: libFd.FlightInfo } = {};
 
-export const getCurrencies = async (): Promise<libFd.Currencies> => {
-  //  TODO error handling
-  if (currencies && currencies.length) return currencies; // feed from internal storage
-  const response = await fetch(`${c.SERVER_URL}/currencies`);
-  if (!response.ok) throw new Error();
-  const data: string[] = await response.json();
-  currencies = data.map(currency => ({ value: currency, label: currency }));
-  return currencies;
+export const getCurrencies = async (): Promise<libFd.Currencies | null> => {
+  try {
+    //  Check cache
+    if (currencies && currencies.length) return currencies;
+    //  Not cached yet, request data
+    const response = await fetch(`${c.SERVER_URL}/currencies`);
+    if (!response.ok) throw new Error(response.statusText);
+    const data: string[] = await response.json();
+    //  Transform data and update cache
+    currencies = data.map(currency => ({ value: currency, label: currency }));
+    return currencies;
+  } catch (err) {
+    return null;
+  }
 };
 
-export const getAirports = async (): Promise<libFd.Airports> => {
-  //  TODO error handling
-  if (airports && airports.length) return airports; // feed from internal storage
-  const response = await fetch(`${c.SERVER_URL}/airports`);
-  if (!response.ok) throw new Error();
-  const data: [string, string][] = await response.json();
-  airports = data.map(airport => ({ value: airport[0], label: airport[1] }));
-  return airports;
+export const getAirports = async (): Promise<libFd.Airports | null> => {
+  try {
+    //  Check cache
+    if (airports && airports.length) return airports;
+    //  Not cached yet, request data
+    const response = await fetch(`${c.SERVER_URL}/airports`);
+    if (!response.ok) throw new Error(response.statusText);
+    const data: [string, string][] = await response.json();
+    //  Transform data and update cache
+    airports = data.map(airport => ({ value: airport[0], label: airport[1] }));
+    return airports;
+  } catch (err) {
+    return null;
+  }
 };
 
 export const getAirportsPartition = async (
   limit: number,
   offset: number,
   search: string
-): Promise<libFd.Airports> => {
-  //  TODO error handling
-  if (!airports) await getAirports(); // feed from internal storage
-  let partition = airports;
-  if (search) {
-    search = search.toLowerCase();
-    partition = partition.filter(airport =>
-      airport.label.toLowerCase().includes(search)
-    );
+): Promise<libFd.Airports | null> => {
+  try {
+    //  Trigger cache update if empty
+    if (!airports) await getAirports();
+    //  Filter data
+    let partition = airports;
+    if (search) {
+      search = search.toLowerCase();
+      partition = partition.filter(airport =>
+        airport.label.toLowerCase().includes(search)
+      );
+    }
+    return partition.slice(offset, limit + offset);
+  } catch (err) {
+    return null;
   }
-  return partition.slice(offset, limit + offset);
 };
 
-export const postLocaleInfoRequest = async (): Promise<libFd.LocaleInfo> => {
-  //  TODO error handling
-  if (localeInfo) return localeInfo;
-  //  Obtain user's IP
-  const responseIp = await fetch(c.IP_REQUEST_URL);
-  if (!responseIp.ok) throw new Error();
-  const ipInfo = await responseIp.json();
-  if (ipInfo?.ip === undefined) throw new Error();
-  //  Use the IP to obtain locale info
-  const responseLocale = await fetch(`${c.SERVER_URL}/request-locale-info`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ ipAddress: ipInfo.ip }),
-  });
-  if (!responseLocale.ok) throw new Error();
-  const dataLocale: libFd.LocaleInfo = await responseLocale.json();
-  localeInfo = dataLocale;
-  return dataLocale;
-};
+export const postLocaleInfoRequest =
+  async (): Promise<libFd.LocaleInfo | null> => {
+    try {
+      //  Check cache
+      if (localeInfo) return localeInfo;
+      //  Obtain user's IP
+      const responseIp = await fetch(c.IP_REQUEST_URL);
+      if (!responseIp.ok) throw new Error(responseIp.statusText);
+      const ipInfo = await responseIp.json();
+      if (ipInfo?.ip === undefined)
+        throw new Error(`Failed to obtain user's IP`);
+      //  Request data
+      const responseLocale = await fetch(
+        `${c.SERVER_URL}/request-locale-info`,
+        {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ ipAddress: ipInfo.ip }),
+        }
+      );
+      if (!responseLocale.ok) throw new Error(responseLocale.statusText);
+      const dataLocale: libFd.LocaleInfo = await responseLocale.json();
+      //  Update cache
+      localeInfo = dataLocale;
+      return dataLocale;
+    } catch (err) {
+      return null;
+    }
+  };
 
 export const postCheapestFlightsRequest = async (
   requestBody: libFd.CheapestFlightsRequest
-): Promise<libFd.CheapestFlights> => {
-  //  TODO error handling
-  const requestJSON: string = JSON.stringify(requestBody);
-  if (cheapestFlightsCache[requestJSON])
-    return cheapestFlightsCache[requestJSON];
-  const response = await fetch(`${c.SERVER_URL}/request-cheapest-flights`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: requestJSON,
-  });
-  if (!response.ok) throw new Error();
-  const data: libFd.CheapestFlights = await response.json();
-  cheapestFlightsCache[requestJSON] = data;
-  return data;
+): Promise<libFd.CheapestFlights | null> => {
+  try {
+    //  Check cache
+    const requestJSON: string = JSON.stringify(requestBody);
+    if (cheapestFlightsCache[requestJSON])
+      return cheapestFlightsCache[requestJSON];
+    //  Not cached yet, request data
+    const response = await fetch(`${c.SERVER_URL}/request-cheapest-flights`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: requestJSON,
+    });
+    if (!response.ok) throw new Error(response.statusText);
+    const data: libFd.CheapestFlights = await response.json();
+    //  Update cache
+    cheapestFlightsCache[requestJSON] = data;
+    return data;
+  } catch (err) {
+    return null;
+  }
 };
 
 export const postFlightInfoRequest = async (
   requestBody: libFd.FlightInfoRequest
-): Promise<libFd.FlightInfo> => {
-  //  TODO error handling
-  const response = await fetch(`${c.SERVER_URL}/request-flight-info`, {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(requestBody),
-  });
-  if (!response.ok) throw new Error();
-  const data: libFd.FlightInfo = await response.json();
-  return data;
+): Promise<libFd.FlightInfo | null> => {
+  try {
+    //  Check cache
+    const requestJSON: string = JSON.stringify(requestBody);
+    if (flightInfoCache[requestJSON]) return flightInfoCache[requestJSON];
+    //  Not cached yet, request data
+    const response = await fetch(`${c.SERVER_URL}/request-flight-info`, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(requestBody),
+    });
+    if (!response.ok) throw new Error(response.statusText);
+    const data: libFd.FlightInfo = await response.json();
+    //  Update cache
+    flightInfoCache[requestJSON] = data;
+    return data;
+  } catch (err) {
+    return null;
+  }
 };
--- client/src/services/const.service.tsx
+++ client/src/services/const.service.tsx
@@ -1,7 +1,33 @@
+/**
+ * @module
+ * Global storage for public constants.
+ * All objects must be frozen to ensure immutability.
+ * Import module as 'c' (for convenience).
+ *
+ * @author Daniel Maczak / where not specified otherwise
+ * @version 1.0.0
+ */
+
+//  Internal dependencies
 import * as libFd from '../libraries/flightData.service';
 
+/**
+ * API connection constants
+ * @constant SERVER_URL address of your webserver
+ * @constant IP_REQUEST_URL used for Locale Info request which requires user IP
+ */
 export const SERVER_URL: string = `http://localhost:3000`;
 export const IP_REQUEST_URL: string = `https://api.ipify.org?format=json`;
+
+/**
+ * Request options
+ * @constant GLOBAL_LOCALE decided to ignore user's locale due to time constraint
+ * @constant OPTIONS_SHOW_WEEKS weeks to display, value MUST be a number
+ * @constant OPTIONS_TRIP_LENGTH defines return date, value MUST be a number
+ * @constant OPTION_SHOW_WEEKS_DEF default on initial load
+ * @constant OPTION_START_DATE_DEF tomorrow, default on initial load
+ * @constant OPTION_ONE_WAY no-return search, default on initial load
+ */
 export const GLOBAL_LOCALE: string = 'en-US';
 
 export const OPTIONS_SHOW_WEEKS: libFd.Option[] = [
@@ -24,6 +50,7 @@ OPTION_START_DATE_DEF.setDate(OPTION_START_DATE_DEF.getDate() + 1);
 export const OPTION_ONE_WAY: libFd.Option = { value: '-1', label: 'One way' };
 OPTIONS_TRIP_LENGTH.unshift(OPTION_ONE_WAY);
 
+//  Freeze objects!
 Object.freeze(OPTIONS_SHOW_WEEKS);
 Object.freeze(OPTIONS_TRIP_LENGTH);
 Object.freeze(OPTION_SHOW_WEEKS_DEF);
