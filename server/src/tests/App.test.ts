6b0b396821e3e1edb62b6de37d184ea3bc26a987
loic khodarkovsky <lkhodarkovsky@gmail.com>
2024-01-23 18:27:04 +0100

    test: database connection fixed

--- server/src/tests/App.test.ts
+++ server/src/tests/App.test.ts
@@ -5,12 +5,12 @@ import supertest from 'supertest';
 
 import { Options } from 'sequelize';
 import { Sequelize } from 'sequelize-typescript';
-import { Currencies } from '../databases/flightData.database';
+import { Airports, Currencies } from '../databases/flightData.database';
 
 describe('Integration tests', () => {
-  // app.use(router);
   const app = express();
-  app.use(express.json()).use(router);
+  app.use(express.json());
+  app.use(router);
   const request = supertest(app);
 
   let sequelize: Sequelize;
@@ -29,8 +29,10 @@ describe('Integration tests', () => {
     sequelize = new Sequelize(dbInfo);
 
     const initSequelize = async () => {
+      sequelize.addModels([Currencies, Airports]);
       await sequelize.sync();
     };
+
 
     initSequelize();
   });
@@ -62,10 +64,30 @@ describe('Integration tests', () => {
   it('checks currencies table has data', async (): Promise<void> => {
     try {
       const res = await request.get('/currencies');
-      const currencies = await Currencies.findAll();
-      expect(res).toBe(currencies);
+      // const currencies = await Currencies.findAll();
+      expect(res.text.length).toBeGreaterThan(150);
     } catch (error) {
       console.error(error);
     }
   });
+
+  it('checks airports table has data', async (): Promise<void> => {
+    try {
+      const res = await request.get('/airports');
+      // const airports = await Airports.findAll();
+      expect(res.text.length).toBeGreaterThan(4000);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+
+  // it ('checks that it returns an airport', async (): Promise<void> => {
+  //   try {
+  //     const res = await request.post('/city-airport')
+  //     .send({cityName: 'Berlin'});
+  //     console.log('res', res)
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // })
 });
